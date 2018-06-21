ad37a2bb77f3c6da93bbefc0fa873eb4d3ee3744
anzohra@outlook.com <anzohra@outlook.com>
2018-06-21 01:36:59 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,14 +11,18 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 % num2
 end
 
 def square_root(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
+  num1 + ((num2 * num3) / num4)
   #Hint:  __ + (( __ * __ ) / __ )
 end
